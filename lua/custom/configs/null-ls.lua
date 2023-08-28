2e5e4f590c024926e8dcbda1748768a1a0283c7c
Nic Laflamme <nic@laflamme.io>
2023-08-27 22:24:28 -0600

    yolo

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -10,7 +10,7 @@ local sources = {
 
   -- webdev stuff
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "json" } }, -- so prettier works only on these filetypes,
+  b.formatting.prettier,
   b.formatting.rustywind, -- Tailwind
 
   -- Elixir // Erlang
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -4,6 +4,18 @@ local overrides = require "custom.configs.overrides"
 local plugins = {
 
   -- Override plugin definition options
+  --
+  {
+    "zbirenbaum/copilot.lua",
+    -- Lazy load when event occurs. Events are triggered
+    -- as mentioned in:
+    -- https://vi.stackexchange.com/a/4495/20389
+    event = "InsertEnter",
+    -- You can also have it load at immediately at
+    -- startup by commenting above and uncommenting below:
+    -- lazy = false
+    opts = overrides.copilot,
+  },
 
   {
     "neovim/nvim-lspconfig",
